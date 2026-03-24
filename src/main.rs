1b9a49b304a4d07c4f0d84c4bd3fbf503ad462d8
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-24 18:27:20 +1100

    Add smoothing Added smoothing between frames, to ensure the visualiser doesn't jump up and down.

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,28 @@ use std::thread;
 use std::time::Duration;
 use rand::prelude::*;
 use std::io::{self, Write, stdout};
-use crossterm::{QueueableCommand, cursor, style, ExecutableCommand, terminal, queue};
+use crossterm::{QueueableCommand, cursor, style, ExecutableCommand, terminal};
 use crossterm::style::{SetForegroundColor, Color};
 
 fn main() -> io::Result<()> {
     let mut rng = rand::rng();
     let mut stdout = stdout();
 
+    let mut cur_values: Vec<f32> = vec![0f32; 20];
+
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
     loop {
         // TODO: Switch dummy data to FFT values.
-        let values: Vec<u32> = (0..20)
-            .map(|_| rng.random_range(0..100))
+        let target_values: Vec<f32> = (0..20)
+            .map(|_| rng.random_range(0..100) as f32)
             .collect();
 
         let mut lines = vec![String::new(); 16];
 
-        for v in &values {
-            let height = ((v * 16) + 99) / 100;
+        for i in 0..20 {
+            cur_values[i] += ((target_values[i] - cur_values[i]) as f32) * 0.2;
+            let height: u32 = (((cur_values[i] * 16.0) + 99.0) / 100.0) as u32;
             for (e, l) in lines.iter_mut().enumerate() {
                 if height >= 16 - e as u32 {
                     l.push_str("▒▒▒ ");

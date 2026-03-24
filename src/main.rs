585d2272b72793c4b10ecc24e49f17eda92f0b11
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-24 17:53:20 +1100

    Switch to Crossterm Switched to using the crossterm rendering framework to allow for clearing the screen between frames

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::thread;
 use std::time::Duration;
 use rand::prelude::*;
+use std::io::{self, Write, stdout};
+use crossterm::{QueueableCommand, cursor, style, ExecutableCommand, terminal};
+use crossterm::style::Stylize;
 
-
-fn main() {
+fn main() -> io::Result<()> {
     let mut values: Vec<u32>;
     let mut rng = rand::rng();
+    let mut stdout = stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
     loop {
         // TODO: Switch dummy data to FFT values.
         values = (0..20)
             .map(|_| rng.random_range(0..100))
             .collect();
+
         let mut lines = vec![String::new(); 16];
+
         for v in &values {
             let height = (v * 16) / 100;
             for (e, l) in lines.iter_mut().enumerate() {
@@ -22,9 +30,13 @@ fn main() {
                 }
             }
         }
-        for line in lines {
-            println!("{}", line);
+
+        for (e, line) in lines.into_iter().enumerate() {
+            stdout.queue(cursor::MoveTo(0, e as u16))?;
+            stdout.queue(style::PrintStyledContent(line.magenta()))?;
         }
-        thread::sleep(Duration::from_secs(5));
+        
+        stdout.flush()?;
+        thread::sleep(Duration::from_secs(1));
     }
 }

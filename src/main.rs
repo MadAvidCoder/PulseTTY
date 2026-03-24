fba5f05091c0b07088a67e4441e5afe734d01c48
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-24 18:13:23 +1100

    Fix indexing
    FIxed the indexing system to index from the bottom, for more idiomatic behaviour.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use crossterm::{QueueableCommand, cursor, style, ExecutableCommand, terminal};
 use crossterm::style::Stylize;
 
 fn main() -> io::Result<()> {
-    let mut values: Vec<u32>;
     let mut rng = rand::rng();
     let mut stdout = stdout();
 
@@ -14,14 +13,14 @@ fn main() -> io::Result<()> {
 
     loop {
         // TODO: Switch dummy data to FFT values.
-        values = (0..20)
+        let values: Vec<u32> = (0..20)
             .map(|_| rng.random_range(0..100))
             .collect();
 
         let mut lines = vec![String::new(); 16];
 
         for v in &values {
-            let height = (v * 16) / 100;
+            let height = ((v * 16) + 99) / 100;
             for (e, l) in lines.iter_mut().enumerate() {
                 if height >= 16 - e as u32 {
                     l.push_str("▒▒▒ ");
@@ -37,6 +36,6 @@ fn main() -> io::Result<()> {
         }
         
         stdout.flush()?;
-        thread::sleep(Duration::from_secs(1));
+        thread::sleep(Duration::from_millis(50));
     }
 }

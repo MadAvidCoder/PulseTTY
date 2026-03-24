193bcd0c4e1e6829d1342dab076a91d825ad3a54
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-24 17:23:32 +1100

    Implement Column Rendering Implemented a basic script to render the frequency amplitude columns, using dummy data for now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::thread;
+use std::time::Duration;
+use rand::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut values: Vec<u32>;
+    let mut rng = rand::rng();
+    loop {
+        // TODO: Switch dummy data to FFT values.
+        values = (0..20)
+            .map(|_| rng.random_range(0..100))
+            .collect();
+        let mut lines = vec![String::new(); 16];
+        for v in &values {
+            let height = (v * 16) / 100;
+            for (e, l) in lines.iter_mut().enumerate() {
+                if height >= 16 - e as u32 {
+                    l.push_str("▒▒▒ ");
+                } else {
+                    l.push_str("    ");
+                }
+            }
+        }
+        for line in lines {
+            println!("{}", line);
+        }
+        thread::sleep(Duration::from_secs(5));
+    }
 }

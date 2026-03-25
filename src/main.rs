e84fa5f12c5ac06192b0f68d35160949f8a67cdb
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-25 16:09:26 +1100

    Split to modules
    Split the rendering code into a module for better readability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::cmp::max;
+mod render;
+
 use std::thread;
 use std::time::Duration;
 use rand::prelude::*;
-use std::io::{self, Write, stdout};
-use crossterm::{QueueableCommand, cursor, style, ExecutableCommand, terminal};
-use crossterm::style::{SetForegroundColor, Color};
+use std::io::{self, stdout, Write};
+use crossterm::{ExecutableCommand, terminal};
+use symphonia::core::io::MediaSourceStream;
+use std::fs::File;
 
 fn main() -> io::Result<()> {
     let mut rng = rand::rng();
@@ -21,8 +23,6 @@ fn main() -> io::Result<()> {
             .map(|_| rng.random_range(0..100) as f32)
             .collect();
 
-        let mut lines = vec![String::new(); 16];
-
         for i in 0..20 {
             if target_values[i] > cur_values[i] {
                 cur_values[i] += ((target_values[i] - cur_values[i]) as f32) * 0.23;
@@ -33,37 +33,15 @@ fn main() -> io::Result<()> {
             if cur_values[i] > peaks[i] {
                 peaks[i] = cur_values[i];
             } else {
-                peaks[i] -= 0.12
+                peaks[i] -= 0.21
             }
             peaks[i] = peaks[i].max(cur_values[i]);
-
-            let height: u32 = (cur_values[i] / 100.0 * 16f32).round() as u32;
-            let peak_height: u32 = max((peaks[i] / 100.0 * 16f32).round() as u32, height+1);
-
-            for (e, l) in lines.iter_mut().enumerate() {
-                if 16 - e as u32 == peak_height {
-                    l.push_str("▄▄▄ ")
-                } else if 16 - e as u32 <= height {
-                    l.push_str("▒▒▒ ");
-                    // l.push_str("░░░ ")
-                } else {
-                    l.push_str("    ");
-                }
-            }
         }
 
-        for (e, line) in lines.into_iter().enumerate() {
-            stdout.queue(SetForegroundColor(match e {
-                0..=2 => Color::Red,
-                3..=6 => Color::Yellow,
-                7..=15 => Color::Green,
-                _ => Color::White,
-            }))?;
-            stdout.queue(cursor::MoveTo(0, e as u16))?;
-            stdout.queue(style::Print(line))?;
-        }
-        
+        render::draw(&mut stdout, &cur_values, &peaks)?;
+
         stdout.flush()?;
+
         thread::sleep(Duration::from_millis(75));
     }
 }
---
+++ src/render.rs
@@ -0,0 +1,37 @@
+use std::cmp::max;
+use std::io::{self, Write};
+use crossterm::{QueueableCommand, cursor, style};
+use crossterm::style::{SetForegroundColor, Color};
+
+pub fn draw(stdout: &mut impl Write, cur_values: &[f32], peaks: &[f32]) -> io::Result<()> {
+    let mut lines = vec![String::new(); 16];
+
+    for i in 0..cur_values.len() {
+        let height: u32 = (cur_values[i] / 100.0 * 16f32).round() as u32;
+        let peak_height: u32 = max((peaks[i] / 100.0 * 16f32).round() as u32, height+1);
+
+        for (e, l) in lines.iter_mut().enumerate() {
+            if 16 - (e as u32) == peak_height {
+                l.push_str("▄▄▄ ")
+            } else if 16 - (e as u32) <= height {
+                l.push_str("▒▒▒ ");
+                // l.push_str("░░░ ")
+            } else {
+                l.push_str("    ");
+            }
+        }
+    }
+
+    for (e, line) in lines.into_iter().enumerate() {
+        stdout.queue(SetForegroundColor(match e {
+            0..=2 => Color::Red,
+            3..=6 => Color::Yellow,
+            7..=15 => Color::Green,
+            _ => Color::White,
+        }))?;
+        stdout.queue(cursor::MoveTo(0, e as u16))?;
+        stdout.queue(style::Print(line))?;
+    }
+
+    Ok(())
+}

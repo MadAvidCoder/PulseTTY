615d935a1059ca97797fa75bc1e55560d6a3f946
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-03-30 15:58:24 +1100

    Prevent Edge-Case Overflows
    Added safe guards to prevent addition overflow which can occur in some edge cases

--- src/render.rs
+++ src/render.rs
@@ -2,13 +2,14 @@ use std::cmp::max;
 use std::io::{self, Write};
 use crossterm::{QueueableCommand, cursor, style};
 use crossterm::style::{SetForegroundColor, Color};
+use rustfft::num_traits::Saturating;
 
 pub fn draw(stdout: &mut impl Write, cur_values: &[f32], peaks: &[f32]) -> io::Result<()> {
     let mut lines = vec![String::new(); 16];
 
     for i in 0..cur_values.len() {
-        let height: u32 = (cur_values[i] / 100.0 * 16f32).round() as u32;
-        let peak_height: u32 = max((peaks[i] / 100.0 * 16f32).round() as u32, height+1);
+        let height: u32 = (cur_values[i] / 100.0 * 16f32).round().clamp(0.0, 16.0) as u32;
+        let peak_height: u32 = max((peaks[i] / 100.0 * 16f32).round().clamp(0.0, 16.0) as u32, height.saturating_add(1));
 
         for (e, l) in lines.iter_mut().enumerate() {
             if 16 - (e as u32) == peak_height {
